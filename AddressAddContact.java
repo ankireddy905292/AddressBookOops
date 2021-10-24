ec9c16e9b13b616775f074f9dfdd9d21a3d407cb
ankireddy905292 <91325607+ankireddy905292@users.noreply.github.com>
2021-10-24 15:55:16 +0530

    Add files via upload

---
+++ AddressAddContact.java
@@ -0,0 +1,53 @@
+package com.bridgelabz;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
+
+public class AddressBookMain {
+
+    static Scanner sc = new Scanner(System.in);
+    static List<Contact> list = new LinkedList<Contact>();
+
+    //Created method for adding contact
+    public static void addContact()
+    {
+        System.out.println("Enter your firstName : ");
+        String firstName = sc.nextLine();
+        System.out.println("Enter your lastName : ");
+        String lastName = sc.nextLine();
+        System.out.println("Enter your address : ");
+        String address = sc.nextLine();
+        System.out.println("Enter your city : ");
+        String city = sc.nextLine();
+        System.out.println("Enter your state : ");
+        String state = sc.nextLine();
+        System.out.println("Enter your zipCode : ");
+        String zip = sc.nextLine();
+        System.out.println("Enter your phoneNo : ");
+        long phoneNo = sc.nextLong();
+        System.out.println("Enter your emailId : ");
+        String email = sc.nextLine();
+        Contact obj = new Contact();
+        list.add(obj);
+    }
+
+
+    public static void main(String[] args) {
+
+        AddressBookMain addressBook = new AddressBookMain();
+        //Displaying the welcome message
+        System.out.println("WELCOME TO ADDRESS BOOK PROBLEM");
+        //Adding new contact
+        System.out.println("Enter details of new contact");
+
+        //Creating contact and adding new contact details to the list
+        int count = 1;
+        while (count == 1) {
+            addContact();
+            count--;
+        }
+
+        System.out.println(list); //printing list
+    }
+}
